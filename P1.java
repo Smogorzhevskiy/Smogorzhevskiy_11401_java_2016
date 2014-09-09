10d888f5ca572c95081a28b1a81ffec36cd3a41a
Smogorzhevskii_G <glebsmogor@gmail.com>
2014-09-09 22:03:43 +0400

    Inintial

---
+++ P1.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.*;
+public class P1 {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Введите координаты, через пробел:");
+        double x=sc.nextDouble();
+        double y=sc.nextDouble();
+        if ((x*x+y*y)<1&&(y>x-1)&&(y<-x+1))
+            System.out.print("Точка принадлежит области");
+        else
+            System.out.print("Точка не принадлежит области");
+
+    }
+}
