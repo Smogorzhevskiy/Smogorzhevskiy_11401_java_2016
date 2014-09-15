37e17bd1be668d12e691a66490190b3ba5b00fa9
Smogorzhevskii_G <glebsmogor@gmail.com>
2014-09-15 06:51:47 +0400

    Ex_5

---
+++ P5.java
@@ -0,0 +1,19 @@
+package com.company;
+import java.io.*;
+import java.util.*;
+public class P5 {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+          int x = sc.nextInt();
+        int n=0;
+        while (x>0);
+        {
+            n = n + (x % 10);
+            x = x / 10;
+
+        }
+        System.out.print(n);
+
+
+    }
+}
